d5570a161c5b8a5f995fcae855e284cb693fd01f
seguidadkaren <liseth_360@hotmail.com>
2018-11-19 16:59:55 -0500

    seguridad
    pagina web seguridad

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+<head> <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+<title>Sistema de Gestion de Aeronaves</title>
+<script language="JavaScript"> 
+setInterval ("window.status = ''",10); 
+</script> 
+<style type="text/css">
+ html, body, div, iframe { margin:0; padding:0; height:100%; } 
+ iframe { display:block; width:100%; border:none; } 
+</style> 
+</head> 
+<body> 
+<?php
+	echo '<iframe src="index.html"></iframe>';
+?>
+</body> 
+ </html> 
+ 
